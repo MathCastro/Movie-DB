17fc120db4f6bf9feb259c9051d67288b29e942b
MathCastro <matheusfk74@gmail.com>
2019-05-25 19:08:56 -0300

    back button on detail screen

--- app/src/main/java/com/supercharge/Homework/view/DetailActivity.kt
+++ app/src/main/java/com/supercharge/Homework/view/DetailActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.supercharge.Homework.R
 import android.content.Intent
+import android.view.MenuItem
 import android.widget.TextView
 import com.supercharge.Homework.controller.MovieController
 import kotlinx.android.synthetic.main.activity_detail.*
@@ -16,10 +17,23 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.title = ""
+
         val intent = intent
         val message = intent.getStringExtra("message")
         print(message)
 
         MovieController().getMovieById(this, message, poster, movie_title, overview, release_date, average)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.getItemId()) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
